4483cb74edfdef0fbce9e4fa0960225709d63d7b
Jack Fuller <jfuller@fastmail.com>
2020-06-15 15:01:25 +0100

    ability to override selected regions

--- packages/table/src/regions.ts
+++ packages/table/src/regions.ts
@@ -81,7 +81,9 @@ export enum TableLoadingOption {
 
 export interface IStyledRegionGroup {
     className?: string;
+    focus?: string;
     regions: IRegion[];
+    selection?: string;
 }
 
 /**
@@ -582,17 +584,27 @@ export class Regions {
         focusedCell: IFocusedCellCoordinates,
     ) {
         let regionGroups: IStyledRegionGroup[] = [];
+        let focusOverridden = false;
+        let selectionOverridden = false;
         if (otherRegions != null) {
+            otherRegions.forEach(region => {
+                if (region.focus) {
+                    focusOverridden = true;
+                }
+                if (region.selection) {
+                    selectionOverridden = true;
+                }
+            });
             regionGroups = regionGroups.concat(otherRegions);
         }
-        if (selectedRegions != null && selectedRegions.length > 0) {
+        if (selectedRegions != null && selectedRegions.length > 0 && !selectionOverridden) {
             regionGroups.push({
                 className: Classes.TABLE_SELECTION_REGION,
                 regions: selectedRegions,
             });
         }
 
-        if (focusedCell != null) {
+        if (focusedCell != null && !focusOverridden) {
             regionGroups.push({
                 className: Classes.TABLE_FOCUS_REGION,
                 regions: [Regions.cell(focusedCell.row, focusedCell.col)],
